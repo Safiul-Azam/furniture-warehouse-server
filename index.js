45c881dd63aa6810f37e1bdbc3ce482d18c32d43
mdsafiulazam <mdriadkhan20@gmail.com>
2022-05-03 16:33:36 +0600

    env require added in index.js

--- index.js
+++ index.js
@@ -1,22 +1,37 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5000
 
 //middle ware
 app.use(cors())
+require('dotenv').config()
 app.use(express.json())
 //collection with mongodb
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASS}@cluster0.6m4yg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.6m4yg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("furnitureWare").collection("furniture");
-  console.log('furniture ware is connected')
-  // perform actions on the collection object
-  client.close();
-});
+
+// used async function
+async function run(){
+    try{
+        await client.connect()
+        const furnitureCollection = client.db("furnitureWare").collection("furniture");
+    
+        app.get('/furniture',async(req, res)=>{
+            console.log(req.query)
+            const query = {}
+            const cursor = furnitureCollection.find(query)
+            const result = await cursor.toArray()
+            res.send(result)
+        })
+    }
+    finally{
+
+    }
+}
+run().catch(console.dir)
 
 
 app.get('/', (req, res)=>{
